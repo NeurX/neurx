edb390e0dde4baf48a77afda659e968d26c86194
Joshua Portelance <joshua_portelance@hotmail.com>
2018-07-09 05:10:44 +0000

    Finished testing training endpoint. Ready to merge.

--- test/neurx/endpoints/train_test.exs
+++ test/neurx/endpoints/train_test.exs
@@ -30,15 +30,24 @@ defmodule TrainTest do
   
   test "Data with Nil PID" do
     try do
-      # Don't need to actual pass a PID.
       Neurx.train(nil, %{}, nil)
       assert(false) # should not reach here.
     rescue
       RuntimeError -> nil
     end
   end
+  
+  test "Nil options" do
+    try do
+      # This will fail because the PID is invalid, only need to test options.
+      Neurx.train(1234, %{}, nil)
+      assert(false) # should not reach here.
+    rescue
+      ArgumentError -> nil
+    end
+  end
 
-  test "Testing Network Training" do
+  test "Testing network training with no hidden layers" do
     # Building the network.
     nn = Neurx.build(%{
       input_layer: 3,
@@ -84,6 +93,100 @@ defmodule TrainTest do
       assert(neuron)
       assert(neuron.activation_fn == nil)
       assert(neuron.learning_rate == 0.1)
+    end)
+    
+    Enum.each(output_layer.neurons, fn pid ->
+      neuron = Neuron.get(pid)
+      assert(neuron)
+      assert(neuron.activation_fn == sigmoid)
+      assert(neuron.learning_rate == 0.1)
+    end)
+
+    # Training on simple dataset.
+    data = TestData.get_simple_training_data()
+    options = %{
+      epochs: 1000,
+      log_freq: 100
+    }
+    tnn = Neurx.train(nn, data, options)
+
+    assert(tnn)
+
+    network = Network.get(tnn)
+    assert(network)
+
+    assert(network.error < 0.01)
+  end
+  
+  test "Testing network training with one hidden layers" do
+    # Building the network.
+    nn = Neurx.build(%{
+      input_layer: 3,
+      output_layer: %{
+        size: 1
+      },
+      hidden_layers: [
+        %{
+          size: 2
+        }
+      ]
+    })
+
+    assert(nn)
+
+    network = Network.get(nn)
+
+    assert(network)
+    assert(network.input_layer)
+    assert(network.output_layer)
+    assert(length(network.hidden_layers) == 1)
+
+    input_layer = Layer.get(network.input_layer)
+    output_layer = Layer.get(network.output_layer)
+
+    sigmoid = Activators.getFunction("Sigmoid")
+    mse = LossFunctions.getFunction("MSE")
+    sgd = Optimizers.getFunction("SGD")
+    assert(sigmoid)
+    assert(mse)
+    assert(sgd)
+
+    assert(network.loss_fn == mse)
+    assert(network.optim_fn == sgd)
+
+    assert(input_layer)
+    assert(length(input_layer.neurons) == 3 + 1)
+    assert(input_layer.activation_fn == nil)
+    assert(input_layer.optim_fn == sgd)
+
+    assert(output_layer)
+    assert(length(output_layer.neurons) == 1)
+    assert(output_layer.activation_fn == sigmoid)
+    assert(output_layer.optim_fn == sgd)
+
+    Enum.each(input_layer.neurons, fn pid ->
+      neuron = Neuron.get(pid)
+      assert(neuron)
+      assert(neuron.activation_fn == nil)
+      assert(neuron.learning_rate == 0.1)
+    end)
+    
+    Enum.each(network.hidden_layers, fn lpid ->
+      layer = Layer.get(lpid)
+      assert(layer)
+      assert(length(layer.neurons) == 3)
+      assert(layer.activation_fn == sigmoid)
+      Enum.each(layer.neurons, fn npid ->
+        neuron = Neuron.get(npid)
+        assert(neuron)
+        assert(neuron.learning_rate == 0.1)
+        assert(neuron.optim_fn == sgd)
+        if neuron.bias? do
+          assert(neuron.activation_fn == nil)
+        else
+          assert(neuron.activation_fn == sigmoid)
+        end
+      end)
     end)
     
     Enum.each(output_layer.neurons, fn pid ->
